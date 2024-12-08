c1012f7c0ef260a1995b7d7f5a6f56606cfe8614
nabed <nassim.abed@kleegroup.com>
2024-12-08 13:45:21 +0100

    SimpleCaptcha: init project

---
+++ simplecaptcha/src/main/java/com/captcha/simplecaptcha/SimplecaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.captcha.simplecaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecaptchaApplication.class, args);
+	}
+
+}
---
+++ simplecaptcha/src/test/java/com/captcha/simplecaptcha/SimplecaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captcha.simplecaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
